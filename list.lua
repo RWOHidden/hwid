c6d093708f8f31a0fb768cd1742df3bf6f9c182e
RWOHidden <127285157+RWOHidden@users.noreply.github.com>
2023-05-25 14:16:29 -0400

    Create list.lua

---
+++ list.lua
@@ -0,0 +1,5 @@
+local autoKey = {
+  "97A8FBBD-C814-4042-AB5D-7FF26181B609", --[[ Role: AutoKey | Discord Tag: splash#0220 ]]
+  "00AAAAA0-AA0A-0000-AA0A-0A000AAA00A0", --[[ Role: AutoKey | Discord Tag: user#0000 ]]
+  "00AAAAA0-AA0A-0000-AA0A-0A000AAA00A0", --[[ Role: AutoKey | Discord Tag: user#0000 ]]
+}
